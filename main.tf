fd0853b7269c8062e2b8fd1d152e47e0981b0014
samiali3 <87322514+samiali3@users.noreply.github.com>
2024-05-14 10:51:50 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,11 +15,11 @@ provider "azurerm" {
   skip_provider_registration = "true"
   
   # Connection to Azure
-  subscription_id = var.subscription_id
+#  subscription_id = var.subscription_id
   client_id = var.client_id
   client_secret = var.client_secret
   tenant_id = var.tenant_id
-}
+#}
 
 variable "prefix" {
   default = "terraform"
